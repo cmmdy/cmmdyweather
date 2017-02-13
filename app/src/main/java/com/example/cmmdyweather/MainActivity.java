e32315f805813dd91b4494a2a0beb13cab3c2360
cmmdy <cmmdy@gmail.com>
2017-02-14 04:57:25 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/cmmdyweather/MainActivity.java
+++ app/src/main/java/com/example/cmmdyweather/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.cmmdyweather;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.INTERNET},
+                123);
     }
 }
